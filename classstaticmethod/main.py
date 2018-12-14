0ac9e95bc38ac416452a423f3acb9edefdf8af22
junjie58msp <412512621@qq.com>
2018-12-14 22:05:20 +0800

    test

--- classstaticmethod/main.py
+++ classstaticmethod/main.py
@@ -5,19 +5,25 @@ class A(object):
     def __init__(self, inputvalue):
         self.value = inputvalue
         print("init method")
-    def instance_method(self):
-        print("instance method")
-        print(self.value)
+
     @staticmethod
     def static_function():
         print("static method")
         print(A.num)
-        print(A(4).instance_method())
+
+    def instance_method(self):
+        print("instance method")
+        print(self.value)
+        self.static_function()
+
     @classmethod
     def class_method(cls):
         print("class method")
-        print(A.num)
+        print(cls.num)
         print(cls(5).instance_method())
+
+
 A.static_function()
 A.class_method()
 A(4).instance_method()
+#push to git
--- swiftPro/swiftPro/main.swift
+++ swiftPro/swiftPro/main.swift
@@ -10,3 +10,19 @@ import Foundation
 
 print("Hello, World!")
 
+let one = "hello"
+for i in one{
+    print(i)
+}
+
+print("hello \(one)")
+
+var n=2
+while n<10{
+    n = n*2+1
+}
+print(n)
+
+for i in 0...5{
+    print(i)
+}
